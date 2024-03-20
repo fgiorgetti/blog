5e6d19860d071eaeaae340178b64dedea04dfee6
Fernando Giorgetti <fgiorget@redhat.com>
2024-03-20 09:07:10 -0300

    Updated new-post script to support directories

--- new-post.sh
+++ new-post.sh
@@ -1,18 +1,38 @@
-filename=$1
+OPTSTRING=":d"
+target_dir="content/posts"
 
 function exit_error() {
     echo $*
     exit 1
 }
 
+while getopts ${OPTSTRING} opt; do
+    case ${opt} in
+        d)
+            create_dir=true
+            ;;
+    esac
+done
+
+if [[ ${OPTIND} -gt $# ]]; then
+    echo "Usage: `basename $0` [-d] new-post-name"
+    exit 1
+fi
+filename=${@:$OPTIND:1}
+
 # Validating filename
 ! [[ "${filename}" =~ ^[a-zA-Z0-9-]+$ ]] && exit_error "Invalid filename. Use only letters, digits and dashes."
 
 # Defining the definitive filename to use
-final_name="`date +%Y%m%d-`${filename}"
+if $create_dir; then
+    ${target_dir} += "/${filename}"
+    final_name="${target_dir}/index.md"
+else
+    final_name="${target_dir}/`date +%Y%m%d-`${filename}.md"
+fi
 
 # Preventing a mess
-[[ -f "${final_name}" ]] && exit_error "Post already exist, use a different name"
+[[ -f "${final_name}" ]]  && exit_error "Post already exist, use a different name"
 
 # Creating the new blog post
-hugo new content/posts/${final_name}.md
+hugo new ${final_name}
